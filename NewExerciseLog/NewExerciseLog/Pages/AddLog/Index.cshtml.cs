b5146ea57f48fe18d3065897e0c10825a3acb58c
HDJabs <davisjt7@jacs.sfasu.edu>
2023-03-22 09:41:12 -0500

    added >dbo > tables, added pages.

---
+++ NewExerciseLog/NewExerciseLog/Pages/AddLog/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NewExerciseLog.UI.Pages.AddLog
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ NewExerciseLog/NewExerciseLog/Pages/ViewStats/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NewExerciseLog.UI.Pages.ViewStats
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
