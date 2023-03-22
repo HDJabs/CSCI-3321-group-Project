15cf15451c21581b3951a647be8a8cfe7e50297c
HDJabs <davisjt7@jacs.sfasu.edu>
2023-03-22 11:52:57 -0500

    Did Schema update and comparison

--- ExerciseLog.DB/dbo/Entry.sql
+++ ExerciseLog.DB/dbo/Entry.sql
@@ -1,8 +1,11 @@
-﻿CREATE TABLE [dbo].[Entry]
-(
-	[EntryId] INT NOT NULL PRIMARY KEY, 
-    [ExerciseGoalId] INT NOT NULL, 
-    [EntryDate] DATETIME NOT NULL, 
-    [HoursExercised] INT NOT NULL, 
-    [MinutesExercised] INT NOT NULL
-)
+﻿CREATE TABLE [dbo].[Entry] (
+    [EntryId]          INT      NOT NULL,
+    [ExerciseGoalId]   INT      NOT NULL,
+    [EntryDate]        DATETIME NOT NULL,
+    [HoursExercised]   INT      NOT NULL,
+    [MinutesExercised] INT      NOT NULL,
+    PRIMARY KEY CLUSTERED ([EntryId] ASC),
+    CONSTRAINT [FK_Entry_ExerciseGoal] FOREIGN KEY ([ExerciseGoalId]) REFERENCES [dbo].[ExerciseGoal] ([ExerciseGoalId])
+);
+
+
--- ExerciseLog.DB/dbo/ExerciseGoal.sql
+++ ExerciseLog.DB/dbo/ExerciseGoal.sql
@@ -1,8 +1,12 @@
-﻿CREATE TABLE [dbo].[ExerciseGoal]
-(
-	[ExerciseGoalId] INT NOT NULL PRIMARY KEY, 
-    [ExerciseId] INT NOT NULL, 
-    [UserId] INT NOT NULL, 
-    [Goal] VARCHAR(5) NOT NULL, 
-    [Total] VARCHAR(5) NOT NULL
-)
+﻿CREATE TABLE [dbo].[ExerciseGoal] (
+    [ExerciseGoalId] INT         NOT NULL,
+    [ExerciseId]     INT         NOT NULL,
+    [UserId]         INT         NOT NULL,
+    [Goal]           VARCHAR (5) NOT NULL,
+    [Total]          VARCHAR (5) NOT NULL,
+    PRIMARY KEY CLUSTERED ([ExerciseGoalId] ASC),
+    CONSTRAINT [FK_ExerciseGoal_Exercise] FOREIGN KEY ([ExerciseId]) REFERENCES [dbo].[Exercise] ([ExerciseId]),
+    CONSTRAINT [FK_ExerciseGoal_User] FOREIGN KEY ([UserId]) REFERENCES [dbo].[User] ([UserId])
+);
+
+
