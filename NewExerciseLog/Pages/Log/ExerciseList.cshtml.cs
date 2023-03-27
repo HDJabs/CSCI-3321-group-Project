6a0fdacf0b4000d4ca5627ae2be8d3352851fa7d
HDJabs <davisjt7@jacks.sfasu.edu>
2023-03-27 12:15:36 -0500

    Added LOG. idk why it wasnt added. but here is

---
+++ NewExerciseLog/Pages/Log/ExerciseList.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NewExerciseLog.UI.Pages.Log
+{
+    public class ExerciseListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
