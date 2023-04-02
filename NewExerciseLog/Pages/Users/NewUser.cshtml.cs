bd59c09d0e15e593533fcc04181911c7d5d41bc2
HDJabs <davisjt7@jacs.sfasu.edu>
2023-04-02 16:36:56 -0500

    Connected to SQL and SSMS

--- NewExerciseLog/Pages/Users/NewUser.cshtml.cs
+++ NewExerciseLog/Pages/Users/NewUser.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
 using NewExerciseLog.UI.Models;
 
 namespace NewExerciseLog.UI.Pages.Users
@@ -10,6 +11,50 @@ namespace NewExerciseLog.UI.Pages.Users
         public User NewUser { get; set; } = new User();
         public void OnGet()
         {
+        }
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                /*
+                             * 1. Create a SQL connection object
+                             * 2. Construct a SQL statement
+                             * 3. Create a SQL command object
+                             * 4. Open the SQL connection
+                             * 5. Execute the SQL command
+                             * 6. Close the SQL connection
+                             * 
+                             */
+                Console.WriteLine("in the if statement");
+                using (SqlConnection conn = new SqlConnection(DBHelper.GetConnectionString()))
+                {
+                    // step 1
+                    // step 2
+                    string sql = "INSERT INTO [User] (UserName, UserFirstName, UserLastName, UserPasswordHash, Salt, DateJoined, LastLogIn) " +
+                        "VALUES (@userName, @firstName, @lastName, @passwordHash, @salt, @joined, @lastLog);";
+                    // step 3
+                    SqlCommand cmd = new SqlCommand(sql, conn);
+                    DateTime date = DateTime.Now;
+                    cmd.Parameters.AddWithValue("@userName", NewUser.UserName);
+                    cmd.Parameters.AddWithValue("@firstName", NewUser.FirstName);
+                    cmd.Parameters.AddWithValue("@lastName", NewUser.LastName);
+                    cmd.Parameters.AddWithValue("@passwordHash", NewUser.UserPasswordHash);
+                    cmd.Parameters.AddWithValue("@salt", "salt");
+                    cmd.Parameters.AddWithValue("@joined", date);
+                    cmd.Parameters.AddWithValue("@lastLog", date);
+
+
+
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    return RedirectToPage("HomePage");
+                }
+            }
+            else
+            {
+                Console.WriteLine("returning");
+                return Page();
+            }
         }
     }
 }
--- NewExerciseLog/Pages/Users/UserSignIn.cshtml.cs
+++ NewExerciseLog/Pages/Users/UserSignIn.cshtml.cs
@@ -1,12 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using NewExerciseLog.UI.Models;
+using System.Reflection.Metadata;
 
 namespace NewExerciseLog.UI.Pages.Users
 {
     public class UserSignInModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public User SignInUser { get; set; } = new User();
+        public void OnGet() { 
+        }
+        public IActionResult OnPost()
         {
+            return RedirectToPage("HomePage");
+            
         }
     }
 }
